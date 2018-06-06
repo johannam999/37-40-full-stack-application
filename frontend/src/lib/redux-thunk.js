f520e38138c24c6481beeeb01270b10c8f674720
Joanna Coll <johannam999@gmail.com>
2018-06-05 22:39:46 -0700

    typo

--- frontend/src/lib/redux-thunk.js
+++ frontend/src/lib/redux-thunk.js
@@ -1,5 +1,4 @@
-// we have to do it this way next middleware
-// function is a middleware
+
 export default store => next => action => (
   typeof action === 'function' ? action(store) : next(action)
 );
